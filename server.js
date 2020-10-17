7fa19923239ee4284803d5fdb396a5b195f667a9
luzialeticia <leticialuzia@tutamail.com>
2020-10-16 22:20:36 -0300

    Refatorando código

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./src/app')
-const PORT = 8080
+const PORT = 5555
 
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`))
--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const games = require('./routes/gamesRoutes')
-
-app.use(express.json())
-app.use('/', games)
-
-module.exports = app
